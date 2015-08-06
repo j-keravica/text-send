251fd23eddf995cbc58e4ff466519eac1feabe70
j-keravica <jelena.keravica@gmail.com>
2015-08-06 13:32:07 +0200

    RSpec setup

---
+++ spec/text_send_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+require "./text_send"
+
+
+RSpec.describe TextSend do
+
+  def app
+    TextSend
+  end
+
+  describe "POST /send" do
+    it "sends words and colors"
+  end
+
+end
